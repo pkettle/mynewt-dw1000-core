e5b4ccf133dcbf45c07c1cb53a13792ac6e5aadc
ajaykumar502 <ajay.kumar@pathpartnertech.com>
2018-06-06 11:31:00 +0530

    1. Paul's feedback comments addressed

--- hw/drivers/dw1000/src/dw1000_rng.c
+++ hw/drivers/dw1000/src/dw1000_rng.c
@@ -303,6 +303,8 @@ rng_rx_complete_cb(dw1000_dev_instance_t * inst)
 #if MYNEWT_VAL(DW1000_TIME)
         //If TDMA based ranging is enabled then the device should not go to receive mode again
         //as it needs to range.So the application will take care of going to receive mode or tx mode
+        if(inst->rng->sem.sem_tokens <= 0)
+		os_sem_release(&inst->rng->sem);
         return;
 #else
         if (dw1000_restart_rx(inst, control).start_rx_error)
